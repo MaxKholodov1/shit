8b77fe177559d35f56e798d0293a2258fe172890
MaxKholodov1 <maksholodov8@gmail.com>
2025-03-07 11:56:59 +0300

    i want just save because i will try rewrite GameState and StateManager

---
+++ src/main/java/com/idk/shit/game/LevelManager.java
@@ -0,0 +1,37 @@
+package com.idk.shit.game;
+
+
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
+
+import com.idk.shit.ui.button;
+import com.idk.shit.utils.Colours;
+import com.idk.shit.utils.InputManager;
+
+public class LevelManager extends GameState {
+    private button startButton = new button(0.f, 0.f, 1.5f, 1f, "start",Colours.RED);
+
+
+
+    public LevelManager(long window, InputManager inputManager) {
+        super(window, State._menu_, inputManager);
+        initLevelManager();
+    }
+    private void initLevelManager(){
+        startButton.draw();
+    }
+    @Override
+    public State update(){
+        startButton.update(window);
+        if (startButton.isClicked() || inputManager.isKeyPressed(GLFW_KEY_SPACE)) {
+            this.curState = State._game_;
+            inputManager.cleanup();
+            return this.curState;
+        }
+        return this.curState;
+    }
+    @Override
+    public void render(){
+        startButton.draw();
+    }
+}
